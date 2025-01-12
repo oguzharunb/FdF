b58079c038e3dd0c96b9f1ac50eb0c38b2bd0d48
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2025-01-12 18:20:33 +0300

    mlx quit applied

--- window_management.c
+++ window_management.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: obastug <obastug@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/02 15:09:01 by ebabaogl          #+#    #+#             */
-/*   Updated: 2025/01/12 16:28:36 by obastug          ###   ########.fr       */
+/*   Created: 2025/01/12 16:56:33 by obastug           #+#    #+#             */
+/*   Updated: 2025/01/12 18:06:59 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include "mlx.h"
 #include "window.h"
 #include "fdf.h"
-#include <stdio.h>
 
 void	draw_background(t_vars *vars)
 {
@@ -37,8 +36,6 @@ void	reset_camera(t_vars *vars)
 
 	width_distance = (double)(PADDED_WIDTH) / ((vars->line_len));
 	height_distance = (double)(PADDED_HEIGHT) / ((vars->line_count));
-	printf("width distance: %lf\n", width_distance);
-	printf("height distance: %lf\n", height_distance);
 	vars->x_y_coef = 0;
 	vars->y_z_coef = 0;
 	vars->z_x_coef = 0;
@@ -46,39 +43,40 @@ void	reset_camera(t_vars *vars)
 	vars->animate = 0;
 	vars->map_x = 0;
 	vars->map_y = 0;
-	vars->height = 10;
+	vars->height = 5;
 	if (width_distance > height_distance)
 		vars->distance = height_distance;
 	else
 		vars->distance = width_distance;
 }
 
+#include <stdio.h>
 int	close_win(void *param)
 {
-	t_mlx		*mlx;
+	t_vars		*vars;
 	static int	win_exists = 1;
 
-	mlx = (t_mlx *)param;
-	if (mlx->win_ptr && win_exists)
+	vars = (t_vars *)param;
+	if (win_exists)
 	{
-		mlx_destroy_window(mlx->mlx_ptr, mlx->win_ptr);
-		mlx_loop_end(mlx->mlx_ptr);
+		mlx_loop_end(vars->mlx->mlx_ptr);
 		win_exists = 0;
-		mlx->win_ptr = NULL;
 	}
 	return (0);
 }
 
-#include <unistd.h>
 int	key_handler(int keycode, void *param)
 {
-	t_vars	*vars;
-
+	t_vars			*vars;
+	static int		once_quit = 0;
 	vars = (t_vars *)param;
+	if (once_quit)
+		return (0);
 	if (keycode == ESC_KEY)
 	{
 		vars->animate = 0;
-		close_win(vars->mlx);
+		once_quit = 1;
+		close_win(vars);
 		return (0);
 	}
 	else if (keycode == E_KEY)
@@ -111,8 +109,6 @@ int	key_handler(int keycode, void *param)
 		vars->zoom -= 0.1;
 	else if (keycode == SPACE_KEY)
 		vars->animate = !vars->animate;
-	printf("button pressed: %d\n", keycode);
-	printf("animate: %d\n", vars->animate);
 	draw_background(vars);
 	render_map(vars);
 	return (0);
@@ -128,7 +124,7 @@ int	init_win(t_vars *vars)
 		return (free(vars->mlx->mlx_ptr), -1);
 	vars->mlx->image = mlx_new_image(vars->mlx->mlx_ptr, WIN_WIDTH, WIN_HEIGHT);
 	if (!vars->mlx->image)
-		return (close_win(vars->mlx), -1);
+		return (-1);
 	vars->data_addr = mlx_get_data_addr(vars->mlx->image, &vars->bits_per_pixel, &vars->size_line, &vars->endian);
 	return (0);
 }
