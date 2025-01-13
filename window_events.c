c75dbe1f199137b1aff7a60eacf1972f6811a601
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2025-01-13 15:21:03 +0300

    animation loop deleted

--- window_events.c
+++ window_events.c
@@ -6,13 +6,12 @@
 /*   By: obastug <obastug@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 17:08:29 by obastug           #+#    #+#             */
-/*   Updated: 2025/01/13 15:11:02 by obastug          ###   ########.fr       */
+/*   Updated: 2025/01/13 15:20:39 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include <limits.h>
-#include <unistd.h>
 #include <mlx.h>
 
 void	render_map(t_vars *vars)
@@ -33,22 +32,4 @@ void	render_map(t_vars *vars)
 	}
 	mlx_put_image_to_window(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
 		vars->mlx->image, 0, 0);
-}
-
-int	animation_loop(void *v_vars)
-{
-	t_vars	*vars;
-
-	vars = (t_vars *)v_vars;
-	if (vars->animate)
-	{
-		mlx_clear_window(vars->mlx->mlx_ptr, vars->mlx->win_ptr);
-		vars->x_y_coef += 0.005;
-		vars->y_z_coef += 0.005;
-		vars->z_x_coef += 0.005;
-		draw_background(vars);
-		render_map(vars);
-		usleep(5000);
-	}
-	return (1);
 }
