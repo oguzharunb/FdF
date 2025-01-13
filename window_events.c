a86e087fc6543bfd53f1d6031d715ca7f97c150c
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2025-01-13 15:13:31 +0300

    put info deleted

--- window_events.c
+++ window_events.c
@@ -6,7 +6,7 @@
 /*   By: obastug <obastug@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 17:08:29 by obastug           #+#    #+#             */
-/*   Updated: 2025/01/13 14:39:31 by obastug          ###   ########.fr       */
+/*   Updated: 2025/01/13 15:11:02 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,6 @@
 #include <limits.h>
 #include <unistd.h>
 #include <mlx.h>
-
-void	put_info(t_vars *vars)
-{
-	mlx_string_put(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
-		50, 70, 0xFFFFFF, "Parallel View: U");
-	mlx_string_put(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
-		50, 90, 0xFFFFFF, "Top View: T");
-	mlx_string_put(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
-		50, 110, 0xFFFFFF, "Translate: Arrow Keys");
-	mlx_string_put(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
-		50, 130, 0xFFFFFF, "Rotate x/y: Q-E");
-	mlx_string_put(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
-		50, 150, 0xFFFFFF, "Rotate x/z: A-D");
-	mlx_string_put(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
-		50, 170, 0xFFFFFF, "Rotate y/z: W-S");
-	mlx_string_put(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
-		50, 190, 0xFFFFFF, "Zoom in out: N-M");
-	mlx_string_put(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
-		50, 210, 0xFFFFFF, "Height + -: J-K");
-	mlx_string_put(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
-		50, 230, 0xFFFFFF, "Animate: Space");
-}
 
 void	render_map(t_vars *vars)
 {
@@ -55,7 +33,6 @@ void	render_map(t_vars *vars)
 	}
 	mlx_put_image_to_window(vars->mlx->mlx_ptr, vars->mlx->win_ptr,
 		vars->mlx->image, 0, 0);
-	put_info(vars);
 }
 
 int	animation_loop(void *v_vars)
