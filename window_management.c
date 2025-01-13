8dfb5d23af1db6dfd6cc4a5467d6f1af8322aaaf
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2025-01-13 13:19:00 +0300

    functions moved

--- window_management.c
+++ window_management.c
@@ -6,7 +6,7 @@
 /*   By: obastug <obastug@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 16:56:33 by obastug           #+#    #+#             */
-/*   Updated: 2025/01/13 13:01:40 by obastug          ###   ########.fr       */
+/*   Updated: 2025/01/13 13:16:49 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,28 +29,6 @@ void	draw_background(t_vars *vars)
 	}
 }
 
-void	reset_camera(t_vars *vars)
-{
-	double	width_distance;
-	double	height_distance;
-
-	width_distance = (double)(PADDED_WIDTH) / ((vars->line_len));
-	height_distance = (double)(PADDED_HEIGHT) / ((vars->line_count));
-	vars->x_y_coef = 0;
-	vars->y_z_coef = 0;
-	vars->z_x_coef = 0;
-	vars->zoom = 1;
-	vars->animate = 0;
-	vars->map_x = 0;
-	vars->map_y = 0;
-	vars->height = 5;
-	if (width_distance > height_distance)
-		vars->distance = height_distance;
-	else
-		vars->distance = width_distance;
-}
-
-#include <stdio.h>
 int	close_win(void *param)
 {
 	t_vars		*vars;
@@ -65,21 +43,9 @@ int	close_win(void *param)
 	return (0);
 }
 
-int	key_handler(int keycode, void *param)
+void	key_handler2(int keycode, t_vars *vars)
 {
-	t_vars			*vars;
-	static int		once_quit = 0;
-	vars = (t_vars *)param;
-	if (once_quit)
-		return (0);
-	if (keycode == ESC_KEY)
-	{
-		vars->animate = 0;
-		once_quit = 1;
-		close_win(vars);
-		return (0);
-	}
-	else if (keycode == E_KEY)
+	if (keycode == E_KEY)
 		vars->x_y_coef += 0.1;
 	else if (keycode == Q_KEY)
 		vars->x_y_coef -= 0.1;
@@ -103,12 +69,33 @@ int	key_handler(int keycode, void *param)
 		vars->map_y += 5;
 	else if (keycode == ARROW_DOWN_KEY)
 		vars->map_y -= 5;
+}
+
+int	key_handler(int keycode, void *param)
+{
+	t_vars			*vars;
+	static int		once_quit = 0;
+	vars = (t_vars *)param;
+	if (once_quit)
+		return (0);
+	if (keycode == ESC_KEY)
+	{
+		vars->animate = 0;
+		once_quit = 1;
+		close_win(vars);
+		return (0);
+	}
 	else if (keycode == N_KEY)
 		vars->zoom += 0.1;
 	else if (keycode == M_KEY && vars->zoom > 0.1)
 		vars->zoom -= 0.1;
 	else if (keycode == SPACE_KEY)
 		vars->animate = !vars->animate;
+	else if (keycode == T_KEY)
+		pro_top_view(vars);
+	else if (keycode == U_KEY)
+		pro_parallel(vars);
+	key_handler2(keycode, param);
 	draw_background(vars);
 	render_map(vars);
 	return (0);
