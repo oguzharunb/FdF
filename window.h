49d61474408fca4a4abd1cddc0ddcd9803a8a358
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2025-01-13 13:03:36 +0300

    header moved

---
+++ window.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   window.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obastug <obastug@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/12 17:02:15 by obastug           #+#    #+#             */
+/*   Updated: 2025/01/13 12:43:25 by obastug          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WINDOW_H
+# define WINDOW_H
+
+# define WIN_WIDTH 1920
+# define WIN_HEIGHT 1080
+# define PADDED_WIDTH 1800
+# define PADDED_HEIGHT 900
+
+# define ON_DESTROY 17
+
+# define ESC_KEY 65307
+# define W_KEY 119
+# define A_KEY 97
+# define S_KEY 115
+# define D_KEY 100
+# define J_KEY 106
+# define K_KEY 107
+# define Q_KEY 113
+# define E_KEY 101
+# define ARROT_LEFT_KEY 65361
+# define ARROW_UP_KEY 65362
+# define ARROW_RIGHT_KEY 65363
+# define ARROW_DOWN_KEY 65364
+# define N_KEY 110
+# define M_KEY 109
+# define SPACE_KEY 32
+
+typedef struct	s_mlx
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*image;
+}				t_mlx;
+
+#endif
